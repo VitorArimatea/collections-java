566359f34f86a8e8a1e870a50584ae188e657170
Vitor Arimatea <vitorodev@outlook.com>
2023-10-05 08:39:02 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 0; i <= 10 ; i++) {
+            System.out.println("Contagem até 10 = " + i);
+        }
+    }
+}
